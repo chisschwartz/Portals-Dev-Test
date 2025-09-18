2497c882a0b7065ffb14cbbe7ee05321d4bab2f8
chisschwartz <christopherschwartz559@gmail.com>
2025-09-17 20:39:59 -0500

    moved components to their own folder, created banner for brain break

--- portals-dev-test/src/App.jsx
+++ portals-dev-test/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import CustomerData from "./CustomerData";
-import CustomerTrans from './CustomerTrans';
+import CustomerData from "./Components/CustomerData";
+import CustomerTrans from './Components/CustomerTrans';
+import Banner from './Components/Banner';
 
 function App() {
   const [transaction, setTransaction] = useState(CustomerData)
 
   return (
     <>
+    <Banner />
     <CustomerTrans transaction={transaction}/>
   
     </>
---
+++ portals-dev-test/src/Components/Banner.jsx
@@ -0,0 +1,10 @@
+function Banner() {
+    return(
+        <div>
+            <h1>Point Calculation</h1>
+            <p>Select a user to calculate point total</p>
+        </div>
+    );
+};
+
+export default Banner;
